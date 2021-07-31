b34201503643a4de6044d1d158b9d963f73e8d2d
roxieetc <roxieschnoor@yahoo.com>
2021-07-30 23:00:36 -0700

    declared variables for answer buttons to set up for event listeners.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /* WELCOME SCREEN MODAL */
-
-//declared modal variables for opening, modal content, and closing modal.
-//will be changing to on page load, but want to make sure it works first.
+//declared modal variables for modal content and closing modal.
 const modal = document.getElementById('modal');
 const close = document.getElementById('close');
 
@@ -20,3 +18,13 @@ close.addEventListener('click', closeModal)
 
 //set timeout to have modal pop-up after 1 second
 setTimeout(openModal, 0)
+
+//------------------------------------------------------------------------------------------
+
+/* CORRECT/INCORRECT ANSWER */
+//set variable for all answer buttons to establish eventlisteners for each.
+const button1 = document.getElementById('answer1');
+const button2 = document.getElementById('answer2');
+const button3 = document.getElementById('answer3');
+const button4 = document.getElementById('answer4');
+

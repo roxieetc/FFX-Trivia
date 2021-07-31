3f0e89958586137f093fd1ca96b5d041ef36553d
roxieetc <roxieschnoor@yahoo.com>
2021-07-30 22:33:53 -0700

    Added basic modal to appear on page load for welcome screen. Used code from class example, but will customize to fit trivia game.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello World')
+/* WELCOME SCREEN MODAL */
+
+//declared modal variables for opening, modal content, and closing modal.
+//will be changing to on page load, but want to make sure it works first.
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
+
+//Function to change modal display to block.
+const openModal = () => {
+    modal.style.display = 'block';
+}
+
+//Function to close modal display
+const closeModal = () => {
+    modal.style.display = 'none';
+}
+
+//Adding event listeners to open and close modal
+close.addEventListener('click', closeModal)
+
+//set timeout to have modal pop-up after 1 second
+setTimeout(openModal, 0)

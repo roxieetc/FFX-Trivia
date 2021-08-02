9fc16e5539ef3a679b665033d41a3f95eac12ccf
roxieetc <roxieschnoor@yahoo.com>
2021-08-01 19:42:06 -0700

    added simple chatacter variables and comments

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ const button1 = document.getElementById('answer1');
 const button2 = document.getElementById('answer2');
 const button3 = document.getElementById('answer3');
 const button4 = document.getElementById('answer4');
+
+//set variables for all characters that will be asked about in level 1 (will try to adjust later, if time permits)
+const characterTidus = ['Tidus', 'Swordplay', 'Sword', 'Zanarkand']
+const characterYuna = ['Yuna', 'Summoner', 'Staff', 'Bevelle']
+const characterAuron = ['Auron', 'Bushido', 'Katana', 'Bevelle']
+const characterWakka = ['Wakka', 'Slots', 'Blitzball', 'Besaid']
+const characterRikku = ['Rikku', 'Mix', 'Claws', 'Bikanel Island']
+const characterLulu = ['Lulu', 'Fury', 'Dolls', 'Besaid']
+const characterKimahri = ['Kimahri', 'Ronso Rage', 'Spear', 'Mt. Gagazet']
+const characterSeymour = ['Seymour', 'Requiem', 'Staff', 'Guadosalam']
